0f2c38a26679016c0e87b3a35c74b3f3504187e2
DIEGO MORAIS <diegoalrais@gmail.com>
2020-05-27 18:14:11 -0300

    primeiro commit

---
+++ src/commons/constants/countries.js
@@ -0,0 +1,35 @@
+import BrazilFlag from '../../assets/images/brazil.png'
+import ukFlag from '../../assets/images/uk.png'
+import GermanyFlag from '../../assets/images/germany.png'
+import ItalyFlag from '../../assets/images/italy.png'
+import UsaFlag from '../../assets/images/usa.png'
+
+const COUNTRIES = [
+  {
+    label: 'Alemanha',
+    value: 'germany',
+    flag: GermanyFlag
+  },
+  {
+    label: 'Brasil',
+    value: 'brazil',
+    flag: BrazilFlag
+  },
+  {
+    label: 'Reino Unido',
+    value: 'UK',
+    flag: ukFlag
+  },
+  {
+    label: 'Estados Unidos',
+    value: 'usa',
+    flag: UsaFlag
+  },
+  {
+    label: 'Itália',
+    value: 'italy',
+    flag: ItalyFlag
+  }
+]
+
+export default COUNTRIES
